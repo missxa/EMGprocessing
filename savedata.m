74cc07b15109956d1639f04316becf47366450f3
missxa <aliona.kharchenko@gmail.com>
2017-06-27 22:04:31 +0200

    add preproc steps, start on user input

--- savedata.m
+++ savedata.m
@@ -23,29 +23,33 @@ jtopic = '/myo_blink/joints/lower/angle';
 mtopic = strcat('/myo_blink/muscles/', muscle, '/sensors');
 joint_sub = rossubscriber(jtopic);
 muscle_sub = rossubscriber(mtopic);
+ch = param.channels(muscle);
 
 %% obtain MVC
-MVC = calculateMVC(param.channels(muscle));
-disp('Calibration completed. Starting the experiment in 10 secs.');
-pause(5);
-disp('Starting the experiment');
-pause(5);
-% MVC = 330.0;
+MVC = calculateMVC(ch);
+input('Calibration completed. Pres ENTER to continue to the experiment', 's');
+
 
 %%
 for j=1:length(emg_array)
     [emg_msg,~] = judp('RECEIVE',16571,400);
     emg = jsondecode(char(emg_msg));
-    emg_array(j) = emg(param.channels('biceps'));
+    emg_array(j) = emg(ch(2)) - emg(ch(1));
     %plot(1:j, emg_array(1:j), '-');
     joint_msg = joint_sub.LatestMessage;
     joint_angles(j) = joint_msg.Data;
     forces(j) = muscle_sub.LatestMessage.ElasticDisplacement * 0.2 + 38;
 end
 
-disp('saving data..');
-%%
-data = struct('MVC', MVC, 'EMG', emg_array, 'angle', joint_angles, 'force', forces);
-save(strcat(session, '.mat'), 'data');
+%% apply basic filters 
+disp('preprocessing data..');
+% band-pass
+bfilt_emg =  bandfilter(emg_array',param.bandfilter(1),param.bandfilter(2),param.freq);
+% notch (50 Hz) 
+nfilt_emg = notch(bfilt_emg, param.sampleRate, 50);
 
+%%
+data = struct('MVC', MVC, 'EMG', nfilt_emg, 'angle', joint_angles, 'force', forces);
+save(strcat(session, '.mat'), 'data');
+plot(1:length(nfilt_emg), nfilt_emg);
 end
