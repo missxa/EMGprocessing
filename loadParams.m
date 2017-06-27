65122d4e2bdd2d61904a9cf40234568f7a0846e7
missxa <aliona.kharchenko@gmail.com>
2017-06-27 22:02:01 +0200

    fixed bipolar derivation

--- loadParams.m
+++ loadParams.m
@@ -4,18 +4,18 @@ param.lowfilter = 500;
 param.highfilter = 10;
 param.RMSwindow = 250;
 %param.nbframe = 4000; % number frame needed (interpolation)
-param.trials = 15;
+param.trials = 10;
 param.trialDuration = 2000;
 param.sampleRate = 600;
 param.freq = 600;
-param.mvc_duration = 3; % in seconds
+param.mvc_duration = 10; % in seconds
 param.mvc_trials = 4;
-param.mvc_pause = 10; % seconds for rest between mvc trials
-param.rec_pause = 3;
-param.t_hold_force = 5;
-param.t_relax = 10;
+param.mvc_pause = 3; % seconds for rest between mvc trials
+
+param.t_hold_force = 8;
+param.t_relax = 3;
 % param.method = 'low' ; % RMS ou low
 param.channels = containers.Map;
-param.channels('biceps') = 1;
-param.channels('triceps') = 5;
+param.channels('biceps') = [1,2];
+param.channels('triceps') = [5,6];
 end
