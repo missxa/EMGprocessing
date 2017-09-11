47b0f8332ceda66881b90d9674cd9e772e098d92
missxa <aliona.kharchenko@gmail.com>
2017-09-11 18:03:56 +0200

    online scripts

---
+++ send_activity.m
@@ -0,0 +1,42 @@
+if not(robotics.ros.internal.Global.isNodeActive)
+    rosinit('localhost');
+end
+
+mother = 'db2';
+level = 4;
+x =[-0.0204    0.2127   -0.0461];
+sample=nan(150,1);
+param.RMSwindow = 50;
+topic = strcat('/emg');
+emg_sub = rossubscriber(topic);
+
+a_pub = rospublisher('/activation','std_msgs/Float64');
+msg = rosmessage(a_pub);
+
+while(1)
+    for i=1:150
+        sample(i) = emg_sub.LatestMessage.Data;
+    end
+    
+    sample = lpfilter(sample, 10, Fs/window);
+
+%     [C , L] = wavedec(sample, level , mother);
+%     d4 = detcoef(C,L,level);
+%     input = mean(abs(d4));
+%     msg.Data = x(1)* input^2 + x(2) * input + x(3);
+%     msg.Data = sim(net, input');
+    
+    RMS = nan(size(sample));
+   
+    for k = param.RMSwindow:length(sample)-param.RMSwindow-1
+        RMS(k,:) = rms(sample(k-param.RMSwindow+1:k+param.RMSwindow));
+    end
+    
+    del = isnan(RMS);
+    RMS(del) = [];
+    
+    msg.Data = sim(net, RMS);
+    
+    send(a_pub, msg);
+    i=1;
+end
